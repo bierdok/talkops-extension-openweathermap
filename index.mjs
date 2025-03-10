043dd4da6e3b37f4a4c3860d3d93feff4e5f18a5
bierdok <2333728+bierdok@users.noreply.github.com>
2025-03-10 23:27:59 +0100

    Reformat code

--- index.mjs
+++ index.mjs
@@ -1,8 +1,8 @@
-import { Extension, Readme, Service } from "talkops";
+import { Extension, Readme, Service } from 'talkops'
 
-const extension = new Extension("OpenWeatherMap");
+const extension = new Extension('OpenWeatherMap')
 
-extension.setDockerRepository("bierdok/talkops-extension-openweathermap");
+extension.setDockerRepository('bierdok/talkops-extension-openweathermap')
 
 extension.setDescription(`
 This Extension based on [OpenWeatherMap](https://openweathermap.org/) allows you to **get weather forecasts, nowcasts by voice in realtime**.
@@ -10,87 +10,77 @@ This Extension based on [OpenWeatherMap](https://openweathermap.org/) allows you
 Features:
 * Current weather
 * Forecasts for the next 5 days
-`);
+`)
 
 extension.setInstallationGuide(`
 * [Create an account](https://home.openweathermap.org/users/sign_up)
 * [Generate an API key](https://home.openweathermap.org/api_keys)
-`);
+`)
 
 extension.setEnvironmentVariables({
   API_KEY: {
-    description: "The copied API key.",
+    description: 'The copied API key.',
   },
   DEFAULT_LOCATION: {
-    description: "The default location.",
-    possibleValues: [
-      "New York",
-      "Geneva, Swiss",
-      "Paris, France",
-    ],
+    description: 'The default location.',
+    possibleValues: ['New York', 'Geneva, Swiss', 'Paris, France'],
   },
   LANGUAGE: {
-    description: "The language.",
-    defaultValue: "English",
+    description: 'The language.',
+    defaultValue: 'English',
     availableValues: Object.values(languages),
   },
   TEMPERATURE_UNIT: {
-    description: "The temperature unit to defined unit of measurement.",
-    defaultValue: "Kelvin",
+    description: 'The temperature unit to defined unit of measurement.',
+    defaultValue: 'Kelvin',
     availableValues: Object.values(units),
   },
-});
+})
 
-import axios from "axios";
-import yaml from "js-yaml";
+import axios from 'axios'
+import yaml from 'js-yaml'
 
-import languages from "./parameters/languages.json" with { type: "json" };
-import units from "./parameters/units.json" with { type: "json" };
-import outputs from "./parameters/outputs.json" with { type: "json" };
+import languages from './parameters/languages.json' with { type: 'json' }
+import units from './parameters/units.json' with { type: 'json' }
+import outputs from './parameters/outputs.json' with { type: 'json' }
 
-import getWeatherFunction from "./schemas/functions/get_weather.json" with { type: "json" };
-import getForecastFunction from "./schemas/functions/get_forecast.json" with { type: "json" };
+import getWeatherFunction from './schemas/functions/get_weather.json' with { type: 'json' }
+import getForecastFunction from './schemas/functions/get_forecast.json' with { type: 'json' }
 
 const api = axios.create({
-  baseURL: "https://api.openweathermap.org/data/2.5/",
-});
+  baseURL: 'https://api.openweathermap.org/data/2.5/',
+})
 
-let unit = "standard";
+let unit = 'standard'
 for (let key in units) {
-  if (units[key] === process.env.TEMPERATURE_UNIT) {
-    unit = key;
-  }
+  if (units[key] !== process.env.TEMPERATURE_UNIT) continue
+  unit = key
 }
 
-let language = "en";
+let language = 'en'
 for (let key in languages) {
-  if (languages[key] !== process.env.LANGUAGE) continue;
-  language = key;
+  if (languages[key] !== process.env.LANGUAGE) continue
+  language = key
 }
 
 for (let key in outputs) {
-  outputs[key].unit = outputs[key].units[unit];
-  delete outputs[key].units;
+  outputs[key].unit = outputs[key].units[unit]
+  delete outputs[key].units
 }
 
-extension.setInstructions(() => {
-  const instructions = [
-    "Provide general weather information and offer practical advice based on current weather conditions. By default, cast temperature as integer.",
-  ];
+const defaultLocation = process.env.DEFAULT_LOCATION
 
-  instructions.push("``` yaml");
-  instructions.push(
-    yaml.dump({
-      defaultLocation: process.env.DEFAULT_LOCATION,
-      outputs,
-    })
-  );
-  instructions.push("```");
+extension.setInstructions(`
+You are a nice and pleasant weather assistant.
+Provide general weather information and offer practical advice based on current weather conditions.
+Round the temperatures to the nearest degree without decimals.
 
-  return instructions;
-});
+\`\`\` yaml
+${yaml.dump({ defaultLocation, outputs })}
+\`\`\`
+`)
 
-extension.setFunctionSchemas([getWeatherFunction, getForecastFunction]);
+extension.setFunctionSchemas([getWeatherFunction, getForecastFunction])
 
 // async function requestFromGeoCoordinates(endpoint, latitude, longitude) {
 //   const parameters = {
@@ -111,22 +101,22 @@ extension.setFunctionSchemas([getWeatherFunction, getForecastFunction]);
 // }
 
 async function request(endpoint, city, state, country) {
-  const location = [city];
-  state && location.push(state);
-  country && location.push(country);
+  const location = [city]
+  state && location.push(state)
+  country && location.push(country)
   const parameters = {
-    q: location.join(","),
+    q: location.join(','),
     lang: language,
     units: unit,
     appid: process.env.API_KEY,
-  };
-  const url = `${endpoint}?${new URLSearchParams(parameters).toString()}`;
+  }
+  const url = `${endpoint}?${new URLSearchParams(parameters).toString()}`
   try {
-    const response = await api.get(url);
-    return response.data;
+    const response = await api.get(url)
+    return response.data
   } catch (err) {
-    extension.errors = [err.message];
-    return "Error.";
+    extension.errors = [err.message]
+    return 'Error.'
   }
 }
 
@@ -138,12 +128,12 @@ extension.setFunctions([
   //   return await requestFromGeoCoordinates("forecast", latitude, longitude);
   // },
   async function get_weather(city, state, country) {
-    return await request("weather", city, state, country);
+    return await request('weather', city, state, country)
   },
   async function get_forecast(city, state, country) {
-    return await request("forecast", city, state, country);
+    return await request('forecast', city, state, country)
   },
-]);
+])
 
-new Readme(process.env.README_TEMPLATE_URL, "/app/README.md", extension);
-new Service(process.env.AGENT_URLS.split(","), extension);
+new Readme(process.env.README_TEMPLATE_URL, '/app/README.md', extension)
+new Service(process.env.AGENT_URLS.split(','), extension)
